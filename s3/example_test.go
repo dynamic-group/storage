8fd9dcfba088210b7f14c9910372e8f2bf7639eb
Joakim Gustin <joakim@dynamic-design.se>
2014-08-23 11:28:41 +0200

    Refactor test

--- s3/example_test.go
+++ s3/example_test.go
@@ -3,40 +3,54 @@
 package s3_test
 
 import (
+	"bytes"
 	"io"
 	"log"
-	"os"
+	"testing"
 
 	"github.com/dynamic-design/storage"
 	_ "github.com/dynamic-design/storage/s3"
 )
 
-func ExampleFile() {
+func TestIntegration(t *testing.T) {
 	b, err := storage.Open("s3", "key=AKIAIEXPRCEXGMUEMY4A secret=50qzh2HoqGCLFc8tcvIVRPYjl4VBYqzvSHaxxQsF bucket=uploadservice-test region=ap-southeast-1")
 	catch(err)
 
 	path := "sub/glados.txt"
+	content := []byte("You haven't escaped, you know.\n")
 
+	// Create the file
 	f, err := b.Create(path)
-	catch(err)
-	_, err = f.Write([]byte("You haven't escaped, you know.\n"))
-	catch(err)
-	err = f.Close()
-	catch(err)
+	if err != nil {
+		t.Error(err)
+	}
+	if _, err := f.Write(content); err != nil {
+		t.Error(err)
+	}
+	if err := f.Close(); err != nil {
+		t.Error(err)
+	}
 
+	// Open it
 	f, err = b.Open(path)
-	catch(err)
-	_, err = io.Copy(os.Stdout, f)
-	catch(err)
-	err = f.Close()
-	catch(err)
+	if err != nil {
+		t.Error(err)
+	}
+	var buf bytes.Buffer
+	if _, err := io.Copy(&buf, f); err != nil {
+		t.Error(err)
+	}
+	if err := f.Close(); err != nil {
+		t.Error(err)
+	}
+	if !bytes.Equal(buf.Bytes(), content) {
+		t.Error("read file content is not the same as we stored")
+	}
 
-	URL, err := b.URL(path)
-	catch(err)
-
-	log.Println(URL)
-
-	// Output: You haven't escaped, you know.
+	// Make sure we can create an URL for the file
+	if _, err := b.URL(path); err != nil {
+		t.Error(err)
+	}
 }
 
 func catch(err error) {
