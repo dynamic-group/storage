a3b15ed5876d199a031e172235d233a992f29892
Joakim Gustin <joakim@dynamic-design.se>
2014-08-23 11:06:25 +0200

    Edited test to save to a subfolder

--- s3/example_test.go
+++ s3/example_test.go
@@ -15,19 +15,26 @@ func ExampleFile() {
 	b, err := storage.Open("s3", "key=AKIAIEXPRCEXGMUEMY4A secret=50qzh2HoqGCLFc8tcvIVRPYjl4VBYqzvSHaxxQsF bucket=uploadservice-test region=ap-southeast-1")
 	catch(err)
 
-	f, err := b.Create("glados.txt")
+	path := "sub/glados.txt"
+
+	f, err := b.Create(path)
 	catch(err)
 	_, err = f.Write([]byte("You haven't escaped, you know.\n"))
 	catch(err)
 	err = f.Close()
 	catch(err)
 
-	f, err = b.Open("glados.txt")
+	f, err = b.Open(path)
 	catch(err)
 	_, err = io.Copy(os.Stdout, f)
 	catch(err)
 	err = f.Close()
 	catch(err)
+
+	URL, err := b.URL(path)
+	catch(err)
+
+	log.Println(URL)
 
 	// Output: You haven't escaped, you know.
 }
